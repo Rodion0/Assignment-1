9e64f81d891e3b015f1b32cf2f97ed1c3abc622c
Rodion0 <jtham3000@gmail.com>
2021-02-09 15:45:46 -0500

    09/02/21

--- Trains.cpp
+++ Trains.cpp
@@ -8,6 +8,7 @@
 //--------------------------------------------------------------------
 #include <iostream>
 #include <cstdlib>
+
 using namespace std; 
 
 
@@ -15,12 +16,11 @@ using namespace std;
 
 class car
 {
-private:
+public:
+    car();
     int data; 
     car * next; 
     car * prev;
-public:
-    car();
 };
 
 car::car()
@@ -39,39 +39,54 @@ class train
 {
 private:
     car * front; 
-    car * rear; 
+    car * rear;
+
 public:
     void makeEmptyTrain();
-    bool isEmptyTrain();
+    bool isEmptyTrain(train * input);
     void insertFront(car * n); 
     void insertRear(car * n); 
     void deleteFront();
     void deleteRear();
-    void printTrains();
+    int valueofTrain();
+    //void printTrains(train * input);
 };
 
-void train:makeEmptyTrain(){
+void train::makeEmptyTrain(){
     
 }
 
-bool train:is_EmptyTrain(){
-    return(this->front == this->rear)
+bool train::isEmptyTrain(train * input){
+    return(input->front == input->rear);
 }
 
-void train:insertFront(car * n){
-
-}
-
-void deleteFront(){
+void train::insertFront(car * n){
 
 }
 
-void deleteRear(){
+void train::deleteFront(){
 
 }
 
-void printTrains(){
+void train::deleteRear(){
 
+}
+
+/*void train::printTrains(train * input){ //Instead of having a method inside the train object itself
+    car * temp = new car;                 //Maybe make method that takes array of trains and turn number 
+    int value                             // For loop to increment turn in main 
+    while(temp->next != input->rear){
+        
+    }
+}*/
+
+int train::valueofTrain(){
+    car * temp = front;
+    int value = 0;
+    while(temp->next != rear){
+        value = temp->data + value;
+    }
+    return value;
 }
 
 
@@ -79,7 +94,30 @@ void printTrains(){
 
 //Generate Random Number within bounds
 int generateRandomNumber(){
-     
+     srand(time(0)); 
+     return rand(); 
+}
+
+//Dice Roll Method
+// Mode 1 is 2 sided dice 
+//Mode 2 is n sided dice
+int diceRoll(int mode, int range){
+    int answer = 0;
+    if(mode == 1){
+        answer = (generateRandomNumber() % 2) +1;
+        return answer; 
+    }
+    else{
+        answer = (generateRandomNumber() % range) + 1;
+        return answer;
+    }
+}
+
+void printTrains(train * trains[], int length){
+    for(int i = 0; i < length; i++){
+        int train_counter = 1
+        cout << "train " << train_counter << " value: " << trains[i].valueofTrain() <<endl;
+    }
 }
 
 
